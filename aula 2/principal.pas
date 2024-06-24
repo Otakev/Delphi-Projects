12e96eda9a6af88a071a21b6e0ba3e14e08888b8
Veketo <victhorstos@gmail.com>
2024-06-24 14:14:44 -0300

    Sixth Commit

---
+++ aula 2/principal.pas
@@ -0,0 +1,41 @@
+unit principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm2 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+var
+  Universo: Integer;
+  Valor: Double;//Float
+begin
+  Valor := 14.5;
+  Universo := 42;
+
+  Valor :=42;
+
+  AllocConsole();
+  Write(Valor:0:2);
+
+end;
+
+end.
